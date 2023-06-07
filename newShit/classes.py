e69d322acad90d19d130d02653e49b7ae9ec6f31
classTheTester <edw5050@gmail.com>
2023-06-07 00:17:57 -0400

    Create classes.py

---
+++ newShit/classes.py
@@ -0,0 +1,20 @@
+import time, random
+
+class words:
+    def __init__(self, word):
+        self.accuracy = 0
+        self.word = 0
+        self.len = len(self.word)
+        self.time = checkTime()
+        self.wordList = len(word)
+        self.mistakes = 0
+        self.timeList = []
+    
+    def calculateAccuracy(self):
+        pass
+    def checkAccuracy:
+        pass
+    def checkTime(self):
+        self.timeList.append(time)
+    
+        
